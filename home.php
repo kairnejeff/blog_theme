4c0835cbe8e084433fa22fb25b6eed52f8419586
jing <jing@karinejeff.fr>
2021-07-05 12:00:45 +0200

    "carrousel"

--- home.php
+++ home.php
@@ -7,85 +7,59 @@
             </div>
         </div>
         
-        <!-- <div class="last-stories">
-            <h2 class="block-title">Dernières histories</h2>
-             <div class="card-group last-stories owl-carousel owl-theme">
-                 <?php
-                    $categories = get_categories(); 
-                    foreach($categories as $category){
-                        
+        <div class="container">
+            <h2 class="block-title">Dernières Histoires</h2>
+            <span class="show-all"><a href="">Afficher tout</a></span>
+            <div class="mhn-slide owl-carousel">
+                <?php 
+                    $args = array(
+                        'numberposts'      => 15,
+                        'orderby'          => 'date',
+                        'order'            => 'DESC',
+                        'post_type'        => 'post',
+                    );
+                    $p = get_posts($args);
+                    foreach($p as  $post){
+                        echo "<div class='mhn-item recent-posts'>";
+                        if (has_post_thumbnail( $post->ID ) ){
+                            $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+                            echo "<img src=\"$image[0]\">";
+                        }
+                        echo "<div class='mhn-text'>";
+                        echo "<h3 class='category-title'>".'<a href="' . get_post_permalink( $post->ID ) . '">'.$post->post_title."</a></h3>";
+                        echo "<p class='post-excerpt'>".$post->post_excerpt."</p>";
+                        echo "</div>";
+                        echo "</div>";
                     }
-                 ?>
-                <div class="card story owl-item">
-                    <img class="card-img-top" src=" " alt="Card image cap">
-                    <div class="card-body">
-                    <h5 class="card-title">story title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="card story owl-item">
-                    <img class="card-img-top" src="" alt="Card image cap">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="card story owl-item">
-                    <img class="card-img-top" src="" alt="Card image cap">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
-                <div class="card story owl-item">
-                    <img class="card-img-top" src="" alt="Card image cap">
-                    <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                    </div>
-                </div>
+                ?>
                 
-                <div class="card show-all">
-                    <h5 class="card-title">
-                        <a href="<?php ?>">Afficher tout</a>
-                    </h5>
-                </div>
-                <div class="owl-nav">
-                    <button type="button" role="presentation" class="owl-prev">
-                        <i class="material-icons"></i>
-                    </button>
-                    <button type="button" role="presentation" class="owl-next disabled">
-                         <i class="material-icons"></i>
-                    </button>
-                </div>
-             </div>
-        </div> -->
+			</div>
+		</div>
         
         
         <div class="container">
             <h2 class="block-title">Histoires par sujet</h2>
             <div class="mhn-slide owl-carousel">
-            <?php 
-                $args = array(
-                    'taxonomy' => 'category',
-                    'hide_empty' => 0
-                );
-                $c = get_categories($args);
-                foreach($c as  $cat){
-                    echo "<div class='mhn-item'>";
-                    if (get_field('categorie_image', 'category_'.$cat->term_id)) {
-                        echo "<img src=".get_field('categorie_image', 'category_'.$cat->term_id).">";
+                <?php 
+                    $args = array(
+                        'taxonomy' => 'category',
+                        'hide_empty' => 0,
+                        'exclude' =>1
+                    );
+                    $c = get_categories($args);;
+                    foreach($c as  $cat){
+                        echo "<div class='mhn-item category'>";
+                        if ($img=get_field('categorie_image', 'category_'.$cat->term_id)) {
+                            echo "<img src=".$img['url'].">";
+                        }
+                        echo "<div class='mhn-text'>";
+                        echo "<h3 class='category-title'>".'<a href="' . get_category_link( $category->term_id ) . '">'.$cat->name."</a></h3>";
+                        echo "<p class='category-description'>".$cat->category_description."</p>";
+                        echo "</div>";
+                        echo "</div>";
                     }
-                    echo "<div class='mhn-text'>";
-                    echo "<p>".$cat->category_description."</p>";
-                    echo "</div>";
-                }
-            ?>
-			
+                ?>
+                
 			</div>
 		</div>
 
