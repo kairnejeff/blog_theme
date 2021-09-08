4bdd949159cad8e5d35956006e21968b07c83dc2
Lucie <lucie.lalo@icloud.com>
2021-09-08 14:54:05 +0200

    nouvelle structure du blog

--- web/index.js
+++ web/index.js
@@ -2,6 +2,7 @@ import '/css/homepage.css';
 import '/css/popnewsletter.css';
 import '/css/carousel.css';
 import '/css/page.css';
+import '/css/category.css';
 import '/css/owl.carousel.min.css';
 import '/js/owl.carousel.min.js';
 import '/js/owl-carousel.js';
